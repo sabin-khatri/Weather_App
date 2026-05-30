85fb0e465a8d11a9c1603f41161f5eb850341203
sabin khatri <khatrisabin611@gmail.com>
2026-05-30 09:52:59 +0545

    added animation

---
+++ lib/widget/fade_slide_in.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class FadeSlideIn extends StatefulWidget {
+  final Widget child;
+  final int delayMs;
+  final Duration duration;
+  final double slideOffset;
+
+  const FadeSlideIn({
+    super.key,
+    required this.child,
+    this.delayMs = 0,
+    this.duration = const Duration(milliseconds: 600),
+    this.slideOffset = 24,
+  });
+
+  @override
+  State<FadeSlideIn> createState() => _FadeSlideInState();
+}
+
+class _FadeSlideInState extends State<FadeSlideIn>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _fade;
+  late Animation<Offset> _slide;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: widget.duration);
+    _fade = CurvedAnimation(parent: _controller, curve: Curves.easeOutCubic);
+    _slide = Tween<Offset>(
+      begin: Offset(0, widget.slideOffset / 100),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOutCubic));
+
+    Future.delayed(Duration(milliseconds: widget.delayMs), () {
+      if (mounted) _controller.forward();
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: _fade,
+      child: SlideTransition(position: _slide, child: widget.child),
+    );
+  }
+}
